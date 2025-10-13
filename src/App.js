9acdac87c97eefd3586263085d86b3fbf767f67a
John Robertson <John.Robertson@nmtafe.wa.edu.au>
2025-10-13 12:08:57 +0800

    feat: app.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import React from "react";
 import DateInputComponent from "./components/DateInputComponent.js";
+import ApodForm from "./components/ApodForm";
+import ApodContent from "./components/ApodContent";
 
 function App() {
-  return <DateInputComponent />;
+  // return <DateInputComponent />;
+  return (
+    <div className="App">
+      <section className="section">
+        <div className="container">
+          <ApodForm />
+          <ApodContent />
+        </div>
+      </section>
+    </div>
+  );
 }
 
 export default App;
